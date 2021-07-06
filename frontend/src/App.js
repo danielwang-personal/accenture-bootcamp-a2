1e536140c4d6148ae652118923b79071a0cf6749
danielwang-personal <daniel867181@gmail.com>
2021-07-06 18:24:53 +1000

    sample dataset working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,22 @@
-import React, { useState } from "react";
-import ReactMapGl from "react-map-gl";
+import React, { useState, useEffect, useMemo   } from "react";
+import MapGL, {Source, Layer} from "react-map-gl";
+import {heatmapLayer, unclusteredPointLayer} from './map-style';
+
+function filterFeaturesByDay(featureCollection, time) {
+  const date = new Date(time);
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
+  const features = featureCollection.features.filter(feature => {
+    const featureDate = new Date(feature.properties.time);
+    return (
+      featureDate.getFullYear() === year &&
+      featureDate.getMonth() === month &&
+      featureDate.getDate() === day
+    );
+  });
+  return {type: 'FeatureCollection', features};
+}
 
 function App() {
 
@@ -11,16 +28,51 @@ function App() {
     zoom: 12
   })
 
+  const [allDays, useAllDays] = useState(true);
+  const [timeRange, setTimeRange] = useState([0, 0]);
+  const [selectedTime, selectTime] = useState(0);
+  const [earthquakes, setEarthQuakes] = useState(null);
+
+  useEffect(() => {
+    /* global fetch */
+    fetch('https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson')
+      .then(resp => resp.json())
+      .then(json => {
+        // Note: In a real application you would do a validation of JSON data before doing anything with it,
+        // but for demonstration purposes we ingore this part here and just trying to select needed data...
+        const features = json.features;
+        const endTime = features[0].properties.time;
+        const startTime = features[features.length - 1].properties.time;
+
+        setTimeRange([startTime, endTime]);
+        setEarthQuakes(json);
+        selectTime(endTime);
+      });
+  }, []);
+
+  const data = useMemo(() => {
+    return allDays ? earthquakes : filterFeaturesByDay(earthquakes, selectedTime);
+  }, [earthquakes, allDays, selectedTime]);
+
   return (
-    <ReactMapGl {...viewport} 
+    <MapGL {...viewport} 
     mapboxApiAccessToken="pk.eyJ1IjoiZGFuaWVsLXdhbmcxMyIsImEiOiJja3FyazNqNWozMDRqMm9tbHJrNXF6MXR3In0.87deKFP-tRAVQYv0ftaFwg"
     mapStyle="mapbox://styles/daniel-wang13/ckqrl5x94en2v17no8natcaj9"
     onViewportChange={viewport =>  {
       setViewport(viewport)
     }}
     >
-      markers here
-    </ReactMapGl>
+      {data && (
+        
+          <Source type="geojson" data={data}>
+            <Layer {...unclusteredPointLayer} />
+            <Layer {...heatmapLayer} />
+          </Source>
+        )}
+        
+    </MapGL>
+    
+
   );
 }
 
