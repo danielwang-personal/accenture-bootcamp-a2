ce11ed90a6f4c557b790d4367b55d181f2aefc6c
Francis Dela Cruz <fdc981@gmail.com>
2021-07-06 17:44:03 +0930

    Add data download and output

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 import requests
+import pandas as pd
+
+import urllib
+
+csv_url = "https://data.nsw.gov.au/data/dataset/aefcde60-3b0c-4bc0-9af1-6fe652944ec2/resource/21304414-1ff1-4243-a5d2-f52778048b29/download/confirmed_cases_table1_location.csv"
+data = pd.read_csv(urllib.request.urlopen(csv_url))
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    res = requests.get("https://data.nsw.gov.au/data/api/3/action/datastore_search?resource_id=21304414-1ff1-4243-a5d2-f52778048b29&q=jones")
-    return f"<p>{res.text}</p>"
+    return f"<p>{str(data)}</p>"
