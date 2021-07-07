21e681212035688448a201d97f8c01813edd6a78
danielwang-personal <daniel867181@gmail.com>
2021-07-07 17:58:08 +1000

    enabled cors

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,9 @@ import pandas as pd
 from integration_service import to_geojson
 from retrieval_service import retrieve_df
 from flask import Flask
+from flask_cors import CORS, cross_origin
+
+
 
 curr_date = datetime.date.today()
 csv_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTwXSqlP56q78lZKxc092o6UuIyi7VqOIQj6RM4QmlVPgtJZfbgzv0a3X7wQQkhNu8MFolhVwMy4VnF/pub?gid=0&single=true&output=csv"
@@ -11,6 +14,8 @@ data = pd.read_csv(urllib.request.urlopen(csv_url))
 data = data[data['postcode'] < 9000]
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/")
 def hello_world():
