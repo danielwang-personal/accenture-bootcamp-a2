5547a9adbf8d564b16867c288ac16a9020bcafa9
danielwang-personal <daniel867181@gmail.com>
2021-07-08 11:13:19 +1000

    added pins next to supermarkets

--- frontend/src/ResultsItem.js
+++ frontend/src/ResultsItem.js
@@ -2,16 +2,32 @@ import React from 'react'
 import Card from 'react-bootstrap/Card'
 import Marker, {Source, Layer} from "react-map-gl";
 
+const ICON = `M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3
+  c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9
+  C20.1,15.8,20.2,15.8,20.2,15.7z`;
+const SIZE = 20;
+
 
 export default function ResultsItem({ name, popularity, address }) {
     return (
         <div className='supermarket-result row px-5 py-1'>
             <Card classstyle={{ width: '18rem' , backgroundColor:'white'}}>
                 <Card.Body>
-                    <Card.Title>{name}</Card.Title>
+                    <Card.Title><svg
+                            height={SIZE}
+                            viewBox="0 0 24 24"
+                            style={{
+                                cursor: 'pointer',
+                                fill: '#d00',
+                                stroke: 'none',
+                                transform: `translate(${-SIZE / 2}px,${-SIZE}px)`
+                            }}
+                            >
+                            <path d={ICON} />
+                            </svg> {name} </Card.Title>
                     <Card.Subtitle className="mb-2 text-muted">{address}</Card.Subtitle>
                     <Card.Text>
-                        The supermarket currently has {popularity} people inside
+                        The supermarket currently is currently at {popularity}% its peak hour capacity.
                     </Card.Text>
                 </Card.Body>
             </Card>
